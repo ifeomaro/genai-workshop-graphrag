aa4ed5e0842eed2cb0e490ac78ed6362b1c4e4d2
Ifeoma Okereke <5240961+ifeomaro@users.noreply.github.com>
2025-04-25 08:22:11 +0000

    Part 3

--- 2-neo4j-graphrag/multimodal_app.py
+++ 2-neo4j-graphrag/multimodal_app.py
@@ -16,9 +16,39 @@ driver = GraphDatabase.driver(uri, auth=(username, password))
 
 
 # 1. Initialize the Embedder
-
+IMAGE_EMBEDDING_MODEL = "clip-ViT-B-32"
+embedder = SentenceTransformerEmbeddings(IMAGE_EMBEDDING_MODEL)
 
 # 2. Initialize the VectorCypherRetriever
+POSTER_INDEX_NAME = "moviePosters"
+retrieval_query = "RETURN node.title as title, node.plot as plot, node.poster as posterUrl, score"
 
+def format_record_function(record: neo4j.Record) -> RetrieverResultItem:
+    return RetrieverResultItem(
+        content=f"Movie title: {record.get('title')}, movie plot: {record.get('plot')}",
+        metadata={
+            "title": record.get("title"),
+            "plot": record.get("plot"),
+            "poster": record.get("posterUrl"),
+            "score": record.get("score"),
+        },
+    )
+
+retriever = VectorCypherRetriever(
+    driver,
+    index_name=POSTER_INDEX_NAME,
+    retrieval_query=retrieval_query,
+    result_formatter=format_record_function,
+    embedder=embedder,
+)
+
+query_text = ("Find a movie where in the poster there are only animals without people")
+top_k = 3
+
+result = retriever.search(query_text=query_text, top_k=top_k)
+
+for r in result.items:
+    print(r.content, r.metadata.get("score"))
+    print(r.metadata["poster"])
 
 driver.close()

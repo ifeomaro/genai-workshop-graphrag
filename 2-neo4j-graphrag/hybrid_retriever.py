7767f069569cd21b44d3da98ca6abadfd0db5ee5
Ifeoma Okereke <5240961+ifeomaro@users.noreply.github.com>
2025-04-25 08:19:28 +0000

    Part 2

--- 2-neo4j-graphrag/hybrid_retriever.py
+++ 2-neo4j-graphrag/hybrid_retriever.py
@@ -14,13 +14,30 @@ driver = GraphDatabase.driver(uri, auth=(username, password))
 
 
 # 1. Initialize the Embedder
+from neo4j_graphrag.embeddings.openai import OpenAIEmbeddings
 
+embedder = OpenAIEmbeddings(model="text-embedding-ada-002")
 
 # 2. Initialize the HybridRetriever
+from neo4j_graphrag.retrievers import HybridRetriever
+from neo4j_graphrag.llm import OpenAILLM
 
+retriever = HybridRetriever(
+    driver=driver,
+    vector_index_name="moviePlots",
+    fulltext_index_name="plotFulltext",
+    embedder=embedder,
+    return_properties=["title", "plot"],
+)
 
 
 # 3. Using the Retriever
+from neo4j_graphrag.generation import GraphRAG
 
+llm = OpenAILLM(model_name="gpt-4o", model_params={"temperature": 0})
+rag = GraphRAG(retriever=retriever, llm=llm)
+query_text = "What is the name of the movie set in 1375 in Imperial China?"
+response = rag.search(query_text=query_text, retriever_config={"top_k": 5})
+print(response.answer)
 
 driver.close()
